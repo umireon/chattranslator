036dce8d69d20ffe6d98c9f6bd05dfee3770cc18
Kaito Udagawa <umireon@gmail.com>
2022-05-01 00:26:13 +0900

    Config

--- constants.ts
+++ constants.ts
@@ -1,17 +1,17 @@
 export interface AppContext {
-  readonly translateTextEndpoint: string;
+  readonly translateTextEndpoint: string
 }
 
 export const DEFAULT_CONTEXT: AppContext = {
-  translateTextEndpoint: "a",
-} as const;
+  translateTextEndpoint: 'a',
+} as const
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBhOCCQhHwwR7LBJOOstvdYBdY2PTWa4NA",
-  authDomain: "chattranslatorbot.firebaseapp.com",
-  projectId: "chattranslatorbot",
-  storageBucket: "chattranslatorbot.appspot.com",
-  messagingSenderId: "1034253470102",
-  appId: "1:1034253470102:web:a4567a1b21cc44edafb5b7",
-  measurementId: "G-Q8LDQFFZJS",
-} as const;
+  apiKey: 'AIzaSyBhOCCQhHwwR7LBJOOstvdYBdY2PTWa4NA',
+  authDomain: 'chattranslatorbot.firebaseapp.com',
+  projectId: 'chattranslatorbot',
+  storageBucket: 'chattranslatorbot.appspot.com',
+  messagingSenderId: '1034253470102',
+  appId: '1:1034253470102:web:a4567a1b21cc44edafb5b7',
+  measurementId: 'G-Q8LDQFFZJS',
+} as const
--- index.ts
+++ index.ts
@@ -1,39 +1,39 @@
-import type { HttpFunction } from "@google-cloud/functions-framework";
-import { TranslationServiceClient } from "@google-cloud/translate";
-import { http } from "@google-cloud/functions-framework";
+import type { HttpFunction } from '@google-cloud/functions-framework'
+import { TranslationServiceClient } from '@google-cloud/translate'
+import { http } from '@google-cloud/functions-framework'
 
 const handleCors: HttpFunction = (req, res) => {
-  const { origin } = req.headers;
-  if (typeof origin !== "undefined") {
-    const { hostname } = new URL(origin);
-    if (hostname === "localhost") {
-      res.set("Access-Control-Allow-Origin", "*");
+  const { origin } = req.headers
+  if (typeof origin !== 'undefined') {
+    const { hostname } = new URL(origin)
+    if (hostname === 'localhost') {
+      res.set('Access-Control-Allow-Origin', '*')
     } else {
-      res.set("Access-Control-Allow-Origin", "https://chattalker.web.app");
+      res.set('Access-Control-Allow-Origin', 'https://chattalker.web.app')
     }
   }
 
-  if (req.method === "OPTIONS") {
-    res.set("Access-Control-Allow-Methods", "GET");
-    res.set("Access-Control-Allow-Headers", "Authorization");
-    res.set("Access-Control-Max-Age", "3600");
-    res.status(204).send("");
-    return false;
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET')
+    res.set('Access-Control-Allow-Headers', 'Authorization')
+    res.set('Access-Control-Max-Age', '3600')
+    res.status(204).send('')
+    return false
   }
 
-  return true;
-};
+  return true
+}
 
 interface TranslateLanguageGlossaryConfig {
-  glossary: string;
-  ignoreCase?: boolean;
+  glossary: string
+  ignoreCase?: boolean
 }
 
 interface TranslateLanguageOption {
-  text: string;
-  glossaryConfig?: TranslateLanguageGlossaryConfig;
-  projectId: string;
-  targetLanguageCode: string;
+  text: string
+  glossaryConfig?: TranslateLanguageGlossaryConfig
+  projectId: string
+  targetLanguageCode: string
 }
 
 const translateLanguage = async (
@@ -50,38 +50,38 @@ const translateLanguage = async (
     glossaryConfig,
     parent: `projects/${projectId}/locations/global`,
     targetLanguageCode,
-  });
-  const [{ translatedText }] = response.translations;
-  return translatedText;
-};
+  })
+  const [{ translatedText }] = response.translations
+  return translatedText
+}
 
-http("translate-text", async (req, res) => {
-  if (!handleCors(req, res)) return;
+http('translate-text', async (req, res) => {
+  if (!handleCors(req, res)) return
 
   // Validate environment
-  const { PROJECT_ID } = process.env;
-  if (typeof PROJECT_ID === "undefined")
-    throw new Error("PROJECT_ID not provided");
+  const { PROJECT_ID } = process.env
+  if (typeof PROJECT_ID === 'undefined')
+    throw new Error('PROJECT_ID not provided')
 
   // Validate query
-  if (req.query.keepAlive === "true") {
-    res.status(204).send("");
-    return;
+  if (req.query.keepAlive === 'true') {
+    res.status(204).send('')
+    return
   }
-  if (typeof req.query.text !== "string") {
-    res.status(400).send("Invalid text");
-    return;
+  if (typeof req.query.text !== 'string') {
+    res.status(400).send('Invalid text')
+    return
   }
-  const { text } = req.query;
+  const { text } = req.query
 
   // Translate text
-  const translationClient = new TranslationServiceClient();
+  const translationClient = new TranslationServiceClient()
   const translatedText = await translateLanguage(translationClient, {
     projectId: PROJECT_ID,
-    targetLanguageCode: "en",
+    targetLanguageCode: 'en',
     text,
-  });
+  })
 
   // Compose response
-  res.send({ translatedText });
-});
+  res.send({ translatedText })
+})
