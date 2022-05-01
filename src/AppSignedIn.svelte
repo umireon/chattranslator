66ddf8c18be69317a5906fd610b5c9f7e901aa45
Kaito Udagawa <umireon@gmail.com>
2022-05-02 00:25:50 +0900

    Add files

---
+++ src/AppSignedIn.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import type { Auth, User } from 'firebase/auth'
+  import { connectTwitch, getTwitchLogin } from './service/twitch'
+  import { sendKeepAliveToTextToSpeech } from './service/audio'
+  import { getTwitchToken } from './service/oauth'
+
+  import type { Analytics } from 'firebase/analytics'
+  import Connect from './lib/Connect.svelte'
+  import { DEFAULT_CONTEXT } from '../constants'
+  import type { Firestore } from 'firebase/firestore'
+  import GenerateUrl from './lib/GenerateUrl.svelte'
+  import Logout from './lib/Logout.svelte'
+  import Toastify from 'toastify-js'
+  import type { UserData } from './service/users'
+
+  import 'three-dots/dist/three-dots.min.css'
+  import 'toastify-js/src/toastify.css'
+
+  export let analytics: Analytics
+  export let auth: Auth
+  export let db: Firestore
+
+  export let user: User
+  export let initialUserData: UserData
+
+  const context = DEFAULT_CONTEXT
+
+  async function initializeTwitch () {
+    const token = await getTwitchToken(db, user)
+    if (typeof token === 'undefined') return
+    const login = await getTwitchLogin(DEFAULT_CONTEXT, token)
+      .catch(e => {
+        Toastify({ text: e.toString() }).showToast()
+      })
+    if (typeof login === 'undefined') return
+    connectTwitch({ login, token }, playAudio)
+  }
+
+  initializeTwitch()
+
+  setInterval(() => {
+    sendKeepAliveToTextToSpeech(DEFAULT_CONTEXT, user)
+  }, 60000)
+  sendKeepAliveToTextToSpeech(DEFAULT_CONTEXT, user)
+</script>
+
+<main>
+  <Connect {context} {db} {user} />
+  <GenerateUrl {db} {user} />
+  <Logout {auth} />
+</main>
