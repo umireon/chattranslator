39b903188ffbe76fc77677ce2663f539eda018f4
Kaito Udagawa <umireon@gmail.com>
2022-05-02 01:28:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,6 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         privacypolicy: resolve(__dirname, 'privacypolicy.html'),
         termsofservice: resolve(__dirname, 'termsofservice.html'),
-        twitch: resolve(__dirname, 'twitch.html'),
-        youtube: resolve(__dirname, 'youtube.html'),
       },
       output: {
         manualChunks: {
