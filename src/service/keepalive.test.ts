cf17759e8e70f6ef7237335e16e1aac54847a76b
Kaito Udagawa <umireon@gmail.com>
2022-05-05 21:10:14 +0900

    keepalive.test.ts

---
+++ src/service/keepalive.test.ts
@@ -0,0 +1,24 @@
+import { User } from 'firebase/auth'
+import { setKeepAliveInterval } from './keepalive'
+
+test('setKeepAliveInterval sends requests immediately adn sets interval', async () => {
+  const user = {
+    async getIdToken() {
+      return 'idToken'
+    },
+  } as User
+  const endpoints = ['endpoint1', 'endpoint2']
+  const _fetch = jest
+    .fn()
+    .mockImplementationOnce((input, init) => {
+      expect(input).toBe('endpoint1?keepAlive=true')
+      expect(init).toEqual({ headers: { authorization: `Bearer idToken` } })
+    })
+    .mockImplementationOnce((input, init) => {
+      expect(input).toBe('endpoint2?keepAlive=true')
+      expect(init).toEqual({ headers: { authorization: `Bearer idToken` } })
+    })
+  const timer = setKeepAliveInterval(user, endpoints, _fetch)
+  expect(timer).toBeDefined()
+  clearInterval(timer)
+})
--- src/service/keepalive.ts
+++ src/service/keepalive.ts
@@ -1,17 +1,21 @@
 import { User } from 'firebase/auth'
 
-export const setKeepAliveInterval = (user: User, endpoints: string[]) => {
+export const setKeepAliveInterval = (
+  user: User,
+  endpoints: string[],
+  _fetch = fetch
+) => {
   const query = new URLSearchParams({ keepAlive: 'true' })
   const sendKeepAlive = async () => {
     const idToken = await user.getIdToken()
     for (const endpoint of endpoints) {
-      fetch(`${endpoint}?${query}`, {
+      _fetch(`${endpoint}?${query}`, {
         headers: {
           authorization: `Bearer ${idToken}`,
         },
       })
     }
   }
-  setInterval(sendKeepAlive, 60000)
   sendKeepAlive()
+  return setInterval(sendKeepAlive, 60000)
 }
