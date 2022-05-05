1bb366858c2f95612edfe6c935933b07dbd1e1df
Kaito Udagawa <umireon@gmail.com>
2022-05-05 22:13:50 +0900

    twitch.test.ts

---
+++ common/twitch.test.ts
@@ -0,0 +1,38 @@
+import {
+  TWITCH_USERS_ENDPOINT,
+  getTwitchLogin,
+  validateTwitchUsersResponse,
+} from './twitch'
+
+import type { AppContext } from './constants'
+
+test('validateTwitchUsersResponse validates the response', async () => {
+  const json = { data: [{ login: '' }] }
+  expect(validateTwitchUsersResponse(json)).toBe(true)
+})
+
+test('getTwitchLogin returns login from the response', async () => {
+  const twitchClientId = 'clientId'
+  const context = { twitchClientId } as AppContext
+  const token = 'token'
+  const login = 'login'
+  const _fetch = jest.fn().mockImplementation((input, init) => {
+    expect(input).toBe(TWITCH_USERS_ENDPOINT)
+    expect(init).toEqual({
+      headers: {
+        authorization: `Bearer ${token}`,
+        'client-id': twitchClientId,
+      },
+    })
+    return {
+      async json() {
+        return {
+          data: [{ login }],
+        }
+      },
+      ok: true,
+    }
+  })
+  const actual = await getTwitchLogin(context, token, _fetch)
+  expect(actual).toBe(login)
+})
--- common/twitch.ts
+++ common/twitch.ts
@@ -11,23 +11,24 @@ export interface TwitchUsersResponse {
 export const validateTwitchUsersResponse = (
   arg: any
 ): arg is TwitchUsersResponse => {
-  if (typeof arg === 'undefined' || arg === null) return false
-  return Array.isArray(arg.data)
+  return Array.isArray(arg?.data) && typeof arg.data[0]?.login === 'string'
 }
+
+export const TWITCH_USERS_ENDPOINT = 'https://api.twitch.tv/helix/users'
 
 export const getTwitchLogin = async (
   { twitchClientId }: AppContext,
   token: string,
   _fetch = fetch
 ): Promise<string> => {
-  const response = await _fetch('https://api.twitch.tv/helix/users', {
+  const response = await _fetch(TWITCH_USERS_ENDPOINT, {
     headers: {
-      Authorization: `Bearer ${token}`,
-      'Client-Id': twitchClientId,
+      authorization: `Bearer ${token}`,
+      'client-id': twitchClientId,
     },
   })
   if (!response.ok) throw new Error('Twitch login could not be retrieved!')
-  const json: unknown = await response.json()
+  const json = await response.json()
   if (!validateTwitchUsersResponse(json)) throw new Error('Invalid response')
   const {
     data: [{ login }],
