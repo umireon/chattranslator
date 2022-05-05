0e31a827c2de6340fa01c438018e8a5fbe8e3b54
Kaito Udagawa <umireon@gmail.com>
2022-05-05 21:16:30 +0900

    oauth.test.ts

---
+++ src/service/oauth.test.ts
@@ -0,0 +1,11 @@
+import { generateNonce } from "./oauth"
+
+test('setKeepAliveInterval sends requests immediately adn sets interval', async () => {
+  const _crypto = {
+    getRandomValues(array: Uint32Array): Uint32Array {
+      array[0] = 42
+      return array
+    } 
+  } as Crypto
+  expect(generateNonce(_crypto)).toBe('0000002a')
+})
--- src/service/oauth.ts
+++ src/service/oauth.ts
@@ -16,9 +16,9 @@ export const setTwitchToken = async (
   await setUserData(db, user, { 'twitch-access-token': token })
 }
 
-export const generateNonce = (): string => {
+export const generateNonce = (_crypto = crypto): string => {
   const array = new Uint32Array(1)
-  const generated = crypto.getRandomValues(array)
+  const generated = _crypto.getRandomValues(array)
   const nonce = generated[0].toString(16).padStart(8, '0')
   return nonce
 }
