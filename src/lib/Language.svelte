0039b626d76eb6a5ba1a454f77839cca025468af
Kaito Udagawa <umireon@gmail.com>
2022-05-06 21:00:21 +0900

    Update Language.svelte

--- src/lib/Language.svelte
+++ src/lib/Language.svelte
@@ -9,6 +9,21 @@
 <main>
   <p>
     <label for="targetLanguage">Target Language</label>
-    <input id="targetLanguage" type="text" bind:value={targetLanguageCode} />
+    <input id="targetLanguage" list="targetLanguageList" type="text" bind:value={targetLanguageCode} />
+    <datalist id="targetLanguageList">
+      <option value="de"></option>
+      <option value="en"></option>
+      <option value="es"></option>
+      <option value="fr"></option>
+      <option value="it"></option>
+      <option value="ja"></option>
+      <option value="ko"></option>
+      <option value="pl"></option>
+      <option value="pt"></option>
+      <option value="ru"></option>
+      <option value="th"></option>
+      <option value="tr"></option>
+      <option value="zh"></option>
+    </datalist>
   </p>
 </main>
