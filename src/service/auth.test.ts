a250d253f950c0dcec6681f4aab7a8b946e4e971
Kaito Udagawa <umireon@gmail.com>
2022-05-06 13:32:36 +0900

    Update auth.test.ts

--- src/service/auth.test.ts
+++ src/service/auth.test.ts
@@ -15,8 +15,12 @@ test('authenticateWithToken returns UserCredential', async () => {
   const token = 'token'
   const uid = 'uid'
   const customToken = 'customToken'
-  const responseText = jest.fn().mockResolvedValue(customToken)
-  const response = { ok: true, text: responseText }
+  const response = {
+    ok: true,
+    async text() {
+      return customToken
+    }
+  }
   const _fetch = jest.fn().mockResolvedValue(response)
   const credential = {} as UserCredential
   mockedSignInWithCustomToken.mockResolvedValue(credential)
@@ -27,7 +31,6 @@ test('authenticateWithToken returns UserCredential', async () => {
     _fetch
   )
   expect(_fetch.mock.calls[0][0]).toBe('endpoint?token=token&uid=uid')
-  expect(responseText.mock.calls.length).toBe(1)
   expect(mockedSignInWithCustomToken.mock.calls[0][0]).toBe(auth)
   expect(mockedSignInWithCustomToken.mock.calls[0][1]).toBe(customToken)
   expect(actual).toBe(credential)
