0a940449691a023625e17d05d55f8b143f836f50
Kaito Udagawa <umireon@gmail.com>
2022-05-06 20:23:21 +0900

    Update GenerateUrl.test.ts

--- src/lib/GenerateUrl.test.ts
+++ src/lib/GenerateUrl.test.ts
@@ -1,7 +1,7 @@
 import GenerateUrl from './GenerateUrl.svelte'
 import { render } from '@testing-library/svelte'
 
-test('Connect snapshot', () => {
+test('GenerateUrl snapshot', () => {
   const component = render(GenerateUrl)
   expect(component.container).toMatchSnapshot()
 })
