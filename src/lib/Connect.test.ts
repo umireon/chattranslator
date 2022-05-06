617ca326de3b17362375ab5529fbf547332e49e5
Kaito Udagawa <umireon@gmail.com>
2022-05-06 20:22:00 +0900

    Update Connect.test.ts

--- src/lib/Connect.test.ts
+++ src/lib/Connect.test.ts
@@ -1,7 +1,7 @@
 import Connect from './Connect.svelte'
 import { render } from '@testing-library/svelte'
 
-test('a', () => {
+test('Connect snapshot', () => {
   const component = render(Connect)
   expect(component.container).toMatchSnapshot()
 })
